0c2bb725bda31f7ace23e42d36eeeaea29c40880
fabioh2m <167311722+fabioh2m@users.noreply.github.com>
2024-10-09 06:48:56 -0300

    Initial commit

---
+++ codigo.py
@@ -0,0 +1,7 @@
+# passo 1: Entrar no  sistema da empresa -https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+
+# passo 2: Fazer login
+# passo 3: Importar a base de dados
+# passo 4: Cadastrar 1 produto
+# passo 5: Repetir o processo de cadastro até acabar os produtos
